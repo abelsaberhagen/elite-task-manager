5a553f87a9974766b568d4e0923ce74ebfd72f82
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-04-06 18:58:30 -0400

    app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
 console.log("Hello World!");
 
-let body = document.getElementById("body");
-let addTaskButton = document.getElementById("addTaskButton");
-let taskCollections = document.getElementById("collection-tasks");
+let body = document.querySelector("body");
+let addTaskButton = document.querySelector("#addTaskButton");
+let taskCollections = document.querySelector("#collection-tasks");
+let column = document.querySelector(".column");
 
 addTaskButton.addEventListener('click',function() {
     let newTask = document.createElement( "div" ) ;
     newTask.className = "task";
-	document.taskCollections.appendChild( newTask ) ;
+    newTask.innerText = "Task";
+    newTask.draggable = true;
+    newTask.addEventListener("dragstart", dragstartHandler);
+	taskCollections.appendChild( newTask ) ;
 })
 
+function dragstartHandler(ev) {
+    //ev.dataTransfer.items.add(ev.column.innerText, "text/plain");
+    ev.dataTransfer.setData("text/html", ev.target.outerHTML);
+}
 
+foreach =>
+column.addEventListener("dragover", (ev) => {
+    ev.preventDefault();
+})
+
+column.addEventListener("drop", (ev) => {
+    ev.preventDefault();
+    const data = ev.dataTransfer.getData("text/plain");
+    ev.column.append(data);
+})
 
 
 const task = {name:"task 1", title:"Task1"}
