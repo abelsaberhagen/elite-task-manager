0bccd7360bbd5fd3af97998155fab306860906da
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-04-06 16:08:31 -0400

    app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
-let 
+let body = document.getElementById("body")
+
+DivObject = function() {
+	    this.div = document.createElement( "div" ) ;
+	    document.body.appendChild( this.div ) ;
+}
+
 
 const task = {name:"task 1", title:"Task1"}
