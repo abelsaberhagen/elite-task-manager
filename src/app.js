581606ea08203f337a7f695a77018794af98d6f3
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-04-06 16:31:31 -0400

    task class added

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ console.log("Hello World!");
 let body = document.getElementById("body");
 let addTaskButton = document.getElementById("addTaskButton");
 
-DivObject = function() {
-	    this.div = document.createElement( "div" ) ;
-	    document.body.appendChild( this.div ) ;
-}
+addTaskButton.addEventListener('click',function() {
+    this.div = document.createElement( "div" ) ;
+	document.body.appendChild( this.div ) ;
+})
+
+
 
 
 const task = {name:"task 1", title:"Task1"}
