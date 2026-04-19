7f8f4941a79a813bce2582abbd5f144d27d91284
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-04-19 18:23:02 -0400

    proper color changing

--- src/app.js
+++ src/app.js
@@ -6,21 +6,49 @@ let taskCollections = document.querySelector("#collection-tasks");
 let columns = document.querySelectorAll(".column");
 let draggedTask = null;
 let isDragging = false;
+const colors = ["pink", "orange", "yellow", "green", "purple"];
 
 
 addTaskButton.addEventListener('click',function() {
     let textbox = document.createElement("textarea");
     textbox.type = "text";
 
+    let penImg = document.createElement("img");
+    penImg.src = "pen.png";
+    let editButton = document.createElement("button");
+    editButton.appendChild(penImg);
+
     let newTask = document.createElement( "div" ) ;
     newTask.className = "task";
     newTask.innerText = "Task";
     newTask.draggable = true;
+    newTask.appendChild(editButton);
     newTask.appendChild(textbox);
+
+    editButton.addEventListener("click", editButtonPress(newTask));
     newTask.addEventListener("dragstart", dragstartHandler);
 
 	taskCollections.appendChild( newTask ) ;
 })
+
+function editButtonPress(task) {
+    let colorButtonCollection = document.createElement("div");
+    for (let i = 0; i < 5; i++) {
+        let button = document.createElement("button"); 
+        button.classList.add(colors[i]);
+        button.addEventListener("click", () => {changeColorOfTask(task, colors[i])});
+        colorButtonCollection.appendChild(button);
+    }
+    task.appendChild(colorButtonCollection);
+    
+}
+
+function changeColorOfTask(task, colorClass) {
+    for (let i = 0; i < 5; i++) {
+        task.classList.remove(colors[i]);
+    }
+    task.classList.add(colorClass); 
+}
 
 function dragstartHandler(ev) {
     //ev.dataTransfer.items.add(ev.column.innerText, "text/plain");
