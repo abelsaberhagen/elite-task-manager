e98dd7e69a051ed2a4688dbaaf34b4f9ed5fa91e
Abel Saberhagen <abelsaberhagen@gmail.com>
2026-04-19 16:55:17 -0400

    Proper column highlighting

--- src/app.js
+++ src/app.js
@@ -7,11 +7,16 @@ let columns = document.querySelectorAll(".column");
 let draggedTask = null;
 let isDragging = false;
 
+
 addTaskButton.addEventListener('click',function() {
+    let textbox = document.createElement("textarea");
+    textbox.type = "text";
+
     let newTask = document.createElement( "div" ) ;
     newTask.className = "task";
     newTask.innerText = "Task";
     newTask.draggable = true;
+    newTask.appendChild(textbox);
     newTask.addEventListener("dragstart", dragstartHandler);
 
 	taskCollections.appendChild( newTask ) ;
@@ -19,38 +24,41 @@ addTaskButton.addEventListener('click',function() {
 
 function dragstartHandler(ev) {
     //ev.dataTransfer.items.add(ev.column.innerText, "text/plain");
-    console.log(ev.target.outerHTML);
-    draggedTask = ev.target;
+    console.log(ev.currentTarget.outerHTML);
+    draggedTask = ev.currentTarget;
+    isDragging = true;
     console.log("drag started");
+    console.log(draggedTask);
 }
 
 
 columns.forEach((column) => {
-    column.addEventListener("dragover", (ev) => {
-    ev.preventDefault();
-    console.log("drag over");
-    
-    
 
-    column.addEventListener("mouseenter", () => {
-        if (draggedTask != null) {
+    column.addEventListener("dragover", (ev) => {
+        ev.preventDefault();
+        console.log("drag over");
+
+        if (isDragging) {
             column.classList.add("drag-active-hover");
         }
     });
-    column.addEventListener("mouseleave", () => {
-        if (draggedTask == null) {
-            column.classList.remove("drag-active-hover");
-        }
-    });
+    column.addEventListener("dragleave", (ev) => {
+        ev.preventDefault();
+        column.classList.remove("drag-active-hover");
+    })
+
+    //column.addEventListener("dragLea")
     
     column.addEventListener("drop", (ev) => {
-    ev.preventDefault();
-    ev.target.appendChild(draggedTask);
-    draggedTask = null;
+        ev.preventDefault();
+        ev.currentTarget.appendChild(draggedTask);
+        draggedTask = null;
+        column.classList.remove("drag-active-hover");
+        console.log(draggedTask);
     });
 
     
 });
 
 
-const task = {name:"task 1", title:"Task1"}
+const task = {name:"task 1", title:"Task1"};
